7c9276e1246fe1859d364890ac1798fa29809bb4
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-08-09 01:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (51.31%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -6,15 +6,14 @@ public:
         for(int i=0 ; i<n ; i++){
             pq.push(arr[i]);
         }
-        while(pq.size()>0){
-            if(pq.size()==1) return pq.top();
+        while(pq.size()>1){
             int x = pq.top();
             pq.pop();
             int y = pq.top();
             pq.pop();
-            int ele =abs(x-y);
-            if(ele!=0) pq.push(ele);
+            if(x!=y) pq.push(x-y);
         }
-        return 0;
+        if(pq.size()==1) return pq.top();
+        else return 0;
     }
 };
