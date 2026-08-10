ce2d9956fe5ba074e377b46003b35c3762e6aec2
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-08-11 04:03:48 +0530

    Time: 6 ms (31.22%) | Memory: 11.7 MB (75.63%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(head==NULL) return head;
+        Node* temp = head;
+        while(temp){
+            Node* a = temp->next;
+            if(temp->child){
+                Node* c = temp->child;
+                temp->child = NULL;
+                c = flatten(c); // reccursion
+                temp->next = c;
+                c->prev = temp;
+                while(c->next){
+                    c = c->next;
+                }
+                c->next = a;
+                if(a)a->prev = c;
+                
+            }
+            temp = a;
+        }
+        return head;
+    }
+};
