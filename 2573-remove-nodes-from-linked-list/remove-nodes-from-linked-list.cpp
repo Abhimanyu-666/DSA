51445dd1f654d2a9ea1217d920efeecd15b4c9a8
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-08-19 03:17:56 +0530

    Time: 16 ms (39.38%) | Memory: 161.3 MB (91.64%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        while (cur != nullptr) {
+            ListNode* next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
     ListNode* removeNodes(ListNode* head) {
-        if(head->next == NULL) return head;
-        ListNode* temp = head;
-        vector<int> arr;
-        while (temp) {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
+        // Step 1: reverse the list
+        head = reverseList(head);
 
-        int n = arr.size();
-        vector<int> st;   // vecxtor implemented as stack because using stack the linked list will printed in reverse order
-        for (int i = 0; i < n; i++) {
-            while (st.size()>0 && st.back()<arr[i]) {
-                st.pop_back();
+        // Step 2: single pass, remove nodes smaller than the running max seen so far
+        ListNode* cur = head;
+        int curMax = cur->val;
+
+        while (cur->next != nullptr) {
+            if (cur->next->val < curMax) {
+                cur->next = cur->next->next; // skip / remove it
+            } else {
+                curMax = cur->next->val;
+                cur = cur->next;
             }
-            st.push_back(arr[i]);
         }
 
-        ListNode* dummy = new ListNode(0);
-        ListNode* t = dummy;
-        for (int i=0 ;i<st.size() ; i++) {
-            t->next = new ListNode(st[i]);
-            t = t->next;
-        }
-        return dummy->next;
+        // Step 3: reverse back to restore original order
+        return reverseList(head);
     }
 };
