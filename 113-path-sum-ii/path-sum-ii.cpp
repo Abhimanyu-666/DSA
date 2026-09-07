a21515b10f637c56c5bd8985280dce02d3e269ba
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-07 17:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (39.86%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    
+    vector<vector<int>> ans;
+    vector<int> path;
 
-    void helper(TreeNode* root, int remaining,vector<vector<int>>& ans,vector<int> path) {
-        if (root == NULL)return;
+    void dfs(TreeNode* root, int remaining) {
+        if (root == nullptr)
+            return;
 
         // Choose
         path.push_back(root->val);
         remaining -= root->val;
 
         // Check: must be a leaf
-        if (root->left == NULL && root->right == NULL && remaining == 0) {
+        if (root->left == nullptr &&
+            root->right == nullptr &&
+            remaining == 0) {
+
             ans.push_back(path);
         }
 
         // Explore
-        helper(root->left, remaining , ans,path);
-        helper(root->right, remaining,  ans , path);
+        dfs(root->left, remaining);
+        dfs(root->right, remaining);
 
         // Backtrack
         path.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
-        vector<int> path;
-        helper(root, targetSum, ans , path);
+        dfs(root, targetSum);
         return ans;
     }
 };
