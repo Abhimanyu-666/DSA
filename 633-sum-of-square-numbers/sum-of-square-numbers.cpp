8b3ac386571856dda18205e2659b87f034e4e8ad
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-14 04:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.72%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-
-    bool isPerfectSquare(int n){
-        int root = sqrt(n);
-        if(root*root==n) return true;
-        else return false;
-    }
     bool judgeSquareSum(int c) {
-        int x = 0;
-        int y = c;
-        while(x<=y){
-            if(isPerfectSquare(x) && isPerfectSquare(y)) return true;
-            else if(!isPerfectSquare(y)){
-                y = ((int)sqrt(y)) * ((int)sqrt(y));// make it the previous perfect square
-                x = c - y;  // x + y = c
-            }
-            else{
-                x = ((int)sqrt(x)+1) * ((int)sqrt(x)+1);// make it the next perfect square
-                y = c - x;  // x + y = c
-            }
+        long left = 0;
+        long right = sqrt(c);
+
+        while(left <= right){
+            long sum = left*left + right*right;
+            if(sum == c) return true;
+            else if(sum < c) left++;
+            else right--;
         }
         return false;
     }
