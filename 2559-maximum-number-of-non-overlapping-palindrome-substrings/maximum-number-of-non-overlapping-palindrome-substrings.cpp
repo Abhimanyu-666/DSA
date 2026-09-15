e8ca1dae6692c22579cf0f0fcc94cc2a5e5f0fc5
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-15 07:53:31 +0530

    Time: 75 ms (67.28%) | Memory: 9.6 MB (79.06%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -3,42 +3,29 @@ public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
 
-        // pal[l][r] = whether s[l..r] is a palindrome
-        vector<vector<bool>> pal(n, vector<bool>(n, false));
-
-        for (int len = 1; len <= n; len++) {
-            for (int l = 0; l + len - 1 < n; l++) {
-                int r = l + len - 1;
-
-                if (len == 1) {
-                    pal[l][r] = true;
-                }
-                else if (len == 2) {
-                    pal[l][r] = (s[l] == s[r]);
-                }
-                else {
-                    pal[l][r] = (s[l] == s[r] && pal[l + 1][r - 1]);
-                }
-            }
-        }
-
-        // dp[i] = max number of palindromes in s[0..i-1]
         vector<int> dp(n + 1, 0);
+        vector<bool> prev(n, false), cur(n, false);
 
-        for (int i = 0; i < n; i++) {
-            // Don't use s[i]
-            dp[i + 1] = dp[i];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1];
 
-            // Try every palindrome ending at i
-            for (int j = 0; j <= i; j++) {
-                int len = i - j + 1;
+            fill(cur.begin(), cur.end(), false);
 
-                if (len >= k && pal[j][i]) {
-                    dp[i + 1] = max(dp[i + 1], dp[j] + 1);
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j] &&
+                    (j - i <= 1 || prev[j - 1])) {
+
+                    cur[j] = true;
+
+                    if (j - i + 1 >= k) {
+                        dp[i] = max(dp[i], 1 + dp[j + 1]);
+                    }
                 }
             }
+
+            swap(prev, cur);
         }
 
-        return dp[n];
+        return dp[0];
     }
 };
