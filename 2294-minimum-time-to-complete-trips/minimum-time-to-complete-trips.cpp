e65ba2e140231e3d3a94151efe718298ea50d821
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-15 21:23:25 +0530

    Time: 30 ms (97.32%) | Memory: 98.5 MB (52.50%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    long long check(long long mid,vector<int>& time, int totalTrips){
-        int n = time.size();
+    bool check(long long mid, vector<int>& time, int totalTrips){
         long long trips = 0;
-        for(int i=0 ; i<n ; i++){
-            trips += mid/(long long)time[i];
+        for (int t : time){
+            trips += mid / (long long)t;
+            if (trips >= totalTrips) return true;
         }
-        if(trips >= (long long)totalTrips) return true;
-        else return false;
+        return false;
     }
+
     long long minimumTime(vector<int>& time, int totalTrips) {
-        int n = time.size();
-        int mx = INT_MIN;
-        for(int i=0 ; i<n ; i++){
-            mx = max(mx,time[i]);
-        }
+        int mn = *min_element(time.begin(), time.end());
         long long lo = 1;
-        long long hi = (long long)mx*(long long)totalTrips; 
-        long long ans = -1 ;
-        while(lo<=hi){
-            long long mid = lo + (hi-lo)/2;
-            if(check(mid, time, totalTrips)==true){
+        long long hi = (long long)mn * (long long)totalTrips;
+        long long ans = hi;
+
+        while (lo <= hi){
+            long long mid = lo + (hi - lo) / 2;
+            if (check(mid, time, totalTrips)){
                 ans = mid;
-                hi = mid-1;
+                hi = mid - 1;
             }
-            else lo = mid+1;
+            else lo = mid + 1;
         }
         return ans;
     }
