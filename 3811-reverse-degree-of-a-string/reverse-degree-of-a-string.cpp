3b6b34cba1929e97e641e6f562a5b7c1a3c34855
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-20 09:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -4,8 +4,7 @@ public:
         int n = s.size();
         int sum = 0;
         for(int i=0 ; i<n ; i++){
-            int revPos = 26 - (s[i] - 'a'); // if s[i]=a -> 98 - 98 = 0
-            sum += revPos * (i+1);
+            sum += (26 - (s[i] - 'a')) * (i+1);; // if s[i]=a -> 98 - 98 = 0
         }
         return sum;
     }
