9babca182ee21a400c691938d3d8847cb3fef220
Abhimanyu-666 <amitthakurr121@gmail.com>
2026-09-22 06:09:57 +0530

    Time: 227 ms (80.00%) | Memory: 273.1 MB (70.91%) - LeetSync

--- 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.cpp
+++ 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.cpp
@@ -1,77 +1,103 @@
+class SegmentTree {
+private:
+    static const int MAXK = 6;
+    int k;
+    int n;
+    vector<array<int, MAXK>> tree;
+
+    void makeLeaf(int o, int value) {
+        tree[o].fill(0);
+        int r = value % k;
+        tree[o][r] = 1;
+        tree[o][k] = r;  // mul
+    }
+
+    void mergePre(const array<int, MAXK>& left, const array<int, MAXK>& right,
+                  array<int, MAXK>& result) {
+        result.fill(0);
+
+        int mulL = left[k];
+        int mulR = right[k];
+        result[k] = (mulL * mulR) % k;
+
+        // Case 1: Entirely within the left interval
+        for (int x = 0; x < k; x++) {
+            result[x] = left[x];
+        }
+
+        // Case 2: Contains the entire left interval, followed by a prefix of
+        // the right interval
+        for (int x = 0; x < k; x++) {
+            result[(mulL * x) % k] += right[x];
+        }
+    }
+
+    void maintain(int o) { mergePre(tree[o * 2], tree[o * 2 + 1], tree[o]); }
+
+    void build(const vector<int>& nums, int o, int l, int r) {
+        if (l == r) {
+            makeLeaf(o, nums[l]);
+            return;
+        }
+        int m = (l + r) / 2;
+        build(nums, o * 2, l, m);
+        build(nums, o * 2 + 1, m + 1, r);
+        maintain(o);
+    }
+
+public:
+    SegmentTree(const vector<int>& nums, int k) : k(k), n(nums.size()) {
+        int size = 2 << (int)ceil(log2(n));
+        tree.resize(size);
+        build(nums, 1, 0, n - 1);
+    }
+
+    void update(int o, int l, int r, int index, int value) {
+        if (l == r) {
+            makeLeaf(o, value);
+            return;
+        }
+        int m = (l + r) / 2;
+        if (index <= m)
+            update(o * 2, l, m, index, value);
+        else
+            update(o * 2 + 1, m + 1, r, index, value);
+        maintain(o);
+    }
+
+    array<int, MAXK> query(int o, int l, int r, int L, int R) {
+        if (L <= l && r <= R) {
+            return tree[o];
+        }
+        int m = (l + r) / 2;
+        if (R <= m) {
+            return query(o * 2, l, m, L, R);
+        }
+        if (L > m) {
+            return query(o * 2 + 1, m + 1, r, L, R);
+        }
+        array<int, MAXK> left = query(o * 2, l, m, L, R);
+        array<int, MAXK> right = query(o * 2 + 1, m + 1, r, L, R);
+        array<int, MAXK> result;
+        mergePre(left, right, result);
+        return result;
+    }
+};
+
 class Solution {
 public:
-    int n, k;
-    struct Node {
-        int prod;
-        int cnt[5][5];
-    };
-    vector<Node> tree;
+    vector<int> resultArray(vector<int>& nums, int k,
+                            vector<vector<int>>& queries) {
+        int n = nums.size();
+        SegmentTree seg(nums, k);
+        vector<int> ans;
 
-    Node makeLeaf(int v) {
-        Node node{};
-        node.prod = v % k;
-        for (int s = 0; s < k; s++) {
-            for (int t = 0; t < k; t++) node.cnt[s][t] = 0;
-            int t = (s * node.prod) % k;
-            node.cnt[s][t] = 1;
-        }
-        return node;
-    }
-
-    Node merge(const Node& L, const Node& R) {
-        Node node{};
-        node.prod = (L.prod * R.prod) % k;
-        for (int s = 0; s < k; s++) {
-            int mid = (s * L.prod) % k;
-            for (int t = 0; t < k; t++) {
-                node.cnt[s][t] = L.cnt[s][t] + R.cnt[mid][t];
-            }
-        }
-        return node;
-    }
-
-    void build(int node, int l, int r, vector<int>& nums) {
-        if (l == r) { tree[node] = makeLeaf(nums[l]); return; }
-        int mid = (l + r) / 2;
-        build(2*node, l, mid, nums);
-        build(2*node+1, mid+1, r, nums);
-        tree[node] = merge(tree[2*node], tree[2*node+1]);
-    }
-
-    void update(int node, int l, int r, int idx, int val) {
-        if (l == r) { tree[node] = makeLeaf(val); return; }
-        int mid = (l + r) / 2;
-        if (idx <= mid) update(2*node, l, mid, idx, val);
-        else update(2*node+1, mid+1, r, idx, val);
-        tree[node] = merge(tree[2*node], tree[2*node+1]);
-    }
-
-    Node query(int node, int l, int r, int ql, int qr) {
-        if (ql <= l && r <= qr) return tree[node];
-        int mid = (l + r) / 2;
-        if (qr <= mid) return query(2*node, l, mid, ql, qr);
-        if (ql > mid) return query(2*node+1, mid+1, r, ql, qr);
-        Node leftRes = query(2*node, l, mid, ql, qr);
-        Node rightRes = query(2*node+1, mid+1, r, ql, qr);
-        return merge(leftRes, rightRes);
-    }
-
-    vector<int> resultArray(vector<int>& nums, int k_, vector<vector<int>>& queries) {
-        n = nums.size();
-        k = k_;
-        tree.assign(4 * n, Node());
-        build(1, 0, n - 1, nums);
-
-        int startResidue = 1 % k;   // <-- the fix: reduce 1 mod k
-
-        vector<int> result;
-        result.reserve(queries.size());
         for (auto& q : queries) {
             int index = q[0], value = q[1], start = q[2], x = q[3];
-            update(1, 0, n - 1, index, value);
-            Node res = query(1, 0, n - 1, start, n - 1);
-            result.push_back(res.cnt[startResidue][x]);
+            seg.update(1, 0, n - 1, index, value);
+            auto pre = seg.query(1, 0, n - 1, start, n - 1);
+            ans.push_back(pre[x]);
         }
-        return result;
+        return ans;
     }
 };
